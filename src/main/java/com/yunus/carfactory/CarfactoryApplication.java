b64e67b6f40985985835fb7e00c92c8ff5682265
Yunus Sezgin <yunussezgin8@hotmail.com>
2020-11-06 00:08:04 +0300

    project created

---
+++ src/main/java/com/yunus/carfactory/CarfactoryApplication.java
@@ -0,0 +1,13 @@
+package com.yunus.carfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarfactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarfactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunus/carfactory/CarfactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.carfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarfactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
